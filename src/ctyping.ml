f7756a95022124c44c09f3eb7bff839203380e37
Abel Verley <abelmaxv@gmail.com>
2023-11-09 16:03:58 +0100

    ctyping

--- src/ctyping.ml
+++ src/ctyping.ml
@@ -1,16 +1,8 @@
 open Cast
 
-let (env : var_declaration list) = []
-
-let check_vardec d = match d with 
- |CDECL 
-
-let rec check_vardec_list l = match l with
-  |[] -> ()
-  |t::q -> check_vardec t;
-          check_vardec_list d
-    
 
 let checkfile file = match file with 
-  | CBLOCK (var_decs, loc_cods) -> check_vardec_list
+| CDECL (loc, name, typ) -> checkvar 
+| CFUN (location, name, declist, typ, code) -> 
+
 
